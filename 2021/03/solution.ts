6c8efb80d969d56f5a2aa26081a6fa2e1fb4c48e
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2021-12-12 00:42:22 +0100

    2021: day 3 part 2

--- 2021/03/solution.ts
+++ 2021/03/solution.ts
@@ -1,6 +1,6 @@
 import stream from "stream";
 
-class PartOneSolver extends stream.Transform {
+class Solver extends stream.Transform {
     #avgBits: number[] = [];
     #numOfSamples: number = 0;
 
@@ -9,13 +9,15 @@ class PartOneSolver extends stream.Transform {
         _: BufferEncoding,
         callback: stream.TransformCallback
     ) {
-        chunk
+        const numbers = chunk
             .toString()
             .split('\n')
-            .filter(s => s)
+            .filter(s => s);
+
+        // update avg value for each bit with each number
+        numbers
             .map(numberString =>
-                numberString
-                    .split(''))
+                numberString.split(''))
             .map(digitArray =>
                 digitArray.map(digitString =>
                     Number.parseInt(digitString)))
@@ -33,10 +35,54 @@ class PartOneSolver extends stream.Transform {
             })
         const gammaRate = Number.parseInt(this.#avgBits.map(n => Math.round(n)).join(''), 2);
         const epsilonRate = Number.parseInt(this.#avgBits.map(n => 1 - Math.round(n)).join(''), 2);
-        callback(null, `${gammaRate * epsilonRate}\n`);
+
+        let oxygenGeneratorRatingCandidates = [...numbers];
+        let co2ScrubberRatingCandidates = [...numbers];
+        for (
+            let bit = 0;
+            (bit < numbers[0].length
+             && (oxygenGeneratorRatingCandidates.length > 1
+                || co2ScrubberRatingCandidates.length > 1));
+            bit++
+        ) {
+            if (oxygenGeneratorRatingCandidates.length > 1) {
+                const mostCommonBit = Math.round(
+                    oxygenGeneratorRatingCandidates
+                        .reduce((sum, candidate) =>
+                            sum + Number.parseInt(candidate[bit]),
+                            0)
+                    / oxygenGeneratorRatingCandidates.length
+                );
+                oxygenGeneratorRatingCandidates = oxygenGeneratorRatingCandidates
+                    .filter(candidate =>
+                        Number.parseInt(candidate[bit]) === mostCommonBit
+                    );
+            }
+            if (co2ScrubberRatingCandidates.length > 1) {
+                const leastCommonBit = 1 - Math.round(
+                    co2ScrubberRatingCandidates
+                        .reduce((sum, candidate) =>
+                            sum + Number.parseInt(candidate[bit]),
+                            0)
+                    / co2ScrubberRatingCandidates.length
+                );
+                co2ScrubberRatingCandidates = co2ScrubberRatingCandidates
+                    .filter(candidate =>
+                        Number.parseInt(candidate[bit]) === leastCommonBit
+                    );
+            }
+        }
+        const oxygenGeneratorRating = Number.parseInt(oxygenGeneratorRatingCandidates[0], 2);
+        const co2ScrubberRating = Number.parseInt(co2ScrubberRatingCandidates[0], 2);
+
+        callback(
+            null,
+            `part one: ${gammaRate * epsilonRate}\n`
+            + `part two: ${oxygenGeneratorRating * co2ScrubberRating}\n`
+        );
     }
 }
 
 process.stdin
-    .pipe(new PartOneSolver())
+    .pipe(new Solver())
     .pipe(process.stdout);
